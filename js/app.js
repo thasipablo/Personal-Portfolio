db8d202cd844091552221041dcbdc940c61638c3
codeStackr <rdzaman20000@gmail.com>
2023-06-27 04:08:29 +0600

    Add click event for js

--- js/app.js
+++ js/app.js
@@ -2,3 +2,10 @@
 const hamburgerMenuBtn = document.querySelector('.hamburger-menu-btn');
 const mobileMenu = document.querySelector('.mobile-menu-wrapper');
 const mobileMenuExit = document.querySelector('.menu-enable-icon');
+
+const toggleMobileMenu = () => {
+  mobileMenu.classList.toggle('hide');
+};
+
+hamburgerMenuBtn.addEventListener('click', toggleMobileMenu);
+mobileMenuExit.addEventListener('click', toggleMobileMenu);
