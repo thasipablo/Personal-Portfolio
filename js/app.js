81c3ce24ab5f15cd9b4e5b26cd1875e6ffe7b37b
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-06-28 23:25:29 +0100

    add invalid email format error message

--- js/app.js
+++ js/app.js
@@ -1,70 +1,59 @@
-//#region add validate form
-
-const emailInput = document.querySelector("#email");
-const formtag = document.querySelector(".form1");
-
-formtag.addEventListener("submit", (event) => {
-  if (emailInput.value !== emailInput.value.toLowerCase()) {
-    event.preventDefault();
-    alert("Email must be lowercase");
-  }
-}
-);
-
-//#endregion
-
-
-
 const projectsList = [
   {
-    name: 'Keeping track of hundreds of components',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
-    featuredImage: 'https://img.freepik.com/free-photo/technology-particle-dots-5g-digital-corporate-background_53876-102624.jpg',
-    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    linkToLiveVersion: 'https://thasipablo.github.io',
-    linkToSource: 'https://github.com/thasipablo/thasipablo.github.io',
+    name: "Keeping track of hundreds of components",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    featuredImage:
+      "https://img.freepik.com/free-photo/technology-particle-dots-5g-digital-corporate-background_53876-102624.jpg",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    linkToLiveVersion: "https://thasipablo.github.io",
+    linkToSource: "https://github.com/thasipablo/thasipablo.github.io",
   },
   {
-    name: 'Keeping track of hundreds of components...',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
-    featuredImage: 'https://img.freepik.com/free-photo/technology-particle-dots-5g-digital-corporate-background_53876-102624.jpg',
-    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    linkToLiveVersion: 'https://thasipablo.github.io',
-    linkToSource: 'https://github.com/thasipablo/thasipablo.github.io',
+    name: "Keeping track of hundreds of components...",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    featuredImage:
+      "https://img.freepik.com/free-photo/technology-particle-dots-5g-digital-corporate-background_53876-102624.jpg",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    linkToLiveVersion: "https://thasipablo.github.io",
+    linkToSource: "https://github.com/thasipablo/thasipablo.github.io",
   },
   {
-    name: 'Keeping track of hundreds of components and ...',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
-    featuredImage: 'https://img.freepik.com/free-photo/technology-particle-dots-5g-digital-corporate-background_53876-102624.jpg',
-    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    linkToLiveVersion: 'https://thasipablo.github.io',
-    linkToSource: 'https://github.com/thasipablo/thasipablo.github.io',
+    name: "Keeping track of hundreds of components and ...",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    featuredImage:
+      "https://img.freepik.com/free-photo/technology-particle-dots-5g-digital-corporate-background_53876-102624.jpg",
+    technologies: ["Ruby on Rails", "CSS", "JavaScript", "HTML"],
+    linkToLiveVersion: "https://thasipablo.github.io",
+    linkToSource: "https://github.com/thasipablo/thasipablo.github.io",
   },
 ];
 
 // get needed tags for the mobile menu
-const hamburgerMenuBtn = document.querySelector('.hamburger-menu-btn');
-const mobileMenu = document.querySelector('.mobile-menu-wrapper');
-const mobileMenuExit = document.querySelector('.menu-enable-icon');
-const mobileMenuLinks = document.querySelectorAll('.mobile .navbar-link-item');
+const hamburgerMenuBtn = document.querySelector(".hamburger-menu-btn");
+const mobileMenu = document.querySelector(".mobile-menu-wrapper");
+const mobileMenuExit = document.querySelector(".menu-enable-icon");
+const mobileMenuLinks = document.querySelectorAll(".mobile .navbar-link-item");
 
 const toggleMobileMenu = () => {
-  mobileMenu.classList.toggle('hide');
+  mobileMenu.classList.toggle("hide");
 };
 
-hamburgerMenuBtn.addEventListener('click', toggleMobileMenu);
-mobileMenuExit.addEventListener('click', toggleMobileMenu);
+hamburgerMenuBtn.addEventListener("click", toggleMobileMenu);
+mobileMenuExit.addEventListener("click", toggleMobileMenu);
 
 mobileMenuLinks.forEach((link) => {
-  link.addEventListener('click', toggleMobileMenu);
+  link.addEventListener("click", toggleMobileMenu);
 });
 
 // project card details popup
-const projectContainerTag = document.querySelector('.cards-container');
+const projectContainerTag = document.querySelector(".cards-container");
 
 projectsList.forEach((project) => {
-  const projectCard = document.createElement('article');
-  projectCard.classList.add('work-card');
+  const projectCard = document.createElement("article");
+  projectCard.classList.add("work-card");
   projectCard.innerHTML = `
     <div class="work-card__head"></div>
     <div class="work-card__body">
@@ -81,29 +70,42 @@ projectsList.forEach((project) => {
   projectContainerTag.appendChild(projectCard);
 });
 
-const popupWindow = document.querySelector('.project-card-popup');
-const popupExitBtn = document.querySelector('.project-card-popup .exit-btn');
-const projectCards = document.querySelectorAll('.work-card');
+const popupWindow = document.querySelector(".project-card-popup");
+const popupExitBtn = document.querySelector(".project-card-popup .exit-btn");
+const projectCards = document.querySelectorAll(".work-card");
 
 const populatePopupData = (project) => {
-  popupWindow.querySelector('.project-popup__title').innerHTML = project.name;
-  popupWindow.querySelector('.project-description').innerHTML = project.description;
+  popupWindow.querySelector(".project-popup__title").innerHTML = project.name;
+  popupWindow.querySelector(".project-description").innerHTML =
+    project.description;
 };
 
 const togglePopupWindow = (event) => {
-  const projectCard = event.target.closest('.work-card');
+  const projectCard = event.target.closest(".work-card");
   if (projectCard) {
-    const projectName = projectCard.querySelector('.card-title').textContent;
+    const projectName = projectCard.querySelector(".card-title").textContent;
     const project = projectsList.find((proj) => proj.name === projectName);
     populatePopupData(project);
-    popupWindow.classList.toggle('hide');
+    popupWindow.classList.toggle("hide");
   }
 };
 
-popupExitBtn.addEventListener('click', () => {
-  popupWindow.classList.toggle('hide');
+popupExitBtn.addEventListener("click", () => {
+  popupWindow.classList.toggle("hide");
 });
 
 projectCards.forEach((card) => {
-  card.querySelector('.cta-btn').addEventListener('click', togglePopupWindow);
+  card.querySelector(".cta-btn").addEventListener("click", togglePopupWindow);
+});
+
+//validate form
+const emailInput = document.querySelector("#email");
+const formTag = document.querySelector(".contact-form");
+const errorMsg = document.querySelector(".error-msg");
+
+formTag.addEventListener("submit", (event) => {
+  if (emailInput.value !== emailInput.value.toLowerCase()) {
+    event.preventDefault();
+    errorMsg.innerHTML = "Email must be lowercase";
+  }
 });
