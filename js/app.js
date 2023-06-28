6d84fb6c8753e5b44a3af60c0b75e4d3d725bb8b
thasipablo <thasipablo@gmail.com>
2023-06-28 09:40:35 -0400

    add the generation of project cards from js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
+const projectsList = [
+  {
+    name: 'Keeping track of hundreds of components',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    featuredImage: 'https://img.freepik.com/free-photo/technology-particle-dots-5g-digital-corporate-background_53876-102624.jpg',
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    linkToLiveVersion: 'https://thasipablo.github.io',
+    linkToSource: 'https://github.com/thasipablo/thasipablo.github.io',
+  },
+  {
+    name: 'Keeping track of hundreds of components...',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    featuredImage: 'https://img.freepik.com/free-photo/technology-particle-dots-5g-digital-corporate-background_53876-102624.jpg',
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    linkToLiveVersion: 'https://thasipablo.github.io',
+    linkToSource: 'https://github.com/thasipablo/thasipablo.github.io',
+  },
+];
+
 // get needed tags for the mobile menu
 const hamburgerMenuBtn = document.querySelector('.hamburger-menu-btn');
 const mobileMenu = document.querySelector('.mobile-menu-wrapper');
 const mobileMenuExit = document.querySelector('.menu-enable-icon');
-const mobileMenuLinks = document.querySelectorAll(
-  '.mobile .navbar-link-item',
-);
+const mobileMenuLinks = document.querySelectorAll('.mobile .navbar-link-item');
 
 const toggleMobileMenu = () => {
   mobileMenu.classList.toggle('hide');
@@ -18,6 +35,27 @@ mobileMenuLinks.forEach((link) => {
 });
 
 // project card details popup
+const projectContainerTag = document.querySelector('.cards-container');
+
+projectsList.forEach((project) => {
+  const projectCard = document.createElement('article');
+  projectCard.classList.add('work-card');
+  projectCard.innerHTML = `
+    <div class="work-card__head"></div>
+    <div class="work-card__body">
+      <h3 class="card-title">${project.name}</h3>
+      <ul class="card-btns">
+        <li class="card-btn">${project.technologies[0]}</li>
+        <li class="card-btn">${project.technologies[1]}</li>
+        <li class="card-btn">${project.technologies[2]}</li>
+        <li class="card-btn">${project.technologies[3]}</li>
+      </ul>
+      <div class="cta-btn"><a href="#">See project</a></d>
+    </div>
+  `;
+  projectContainerTag.appendChild(projectCard);
+});
+
 const popupWindow = document.querySelector('.project-card-popup');
 const popupExitBtn = document.querySelector('.project-card-popup .exit-btn');
 const projectCards = document.querySelectorAll('.work-card');
